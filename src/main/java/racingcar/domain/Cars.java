dca4c4fa8de2ef8c6f7cf638021a8e0e3b115b8a
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-12-06 18:45:28 +0900

    feat: 경주 자동차들을 총괄하는 Cars 구현

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,17 @@
+package racingcar.domain;
+
+import java.util.List;
+
+import racingcar.Car;
+
+public class Cars {
+	private final List<Car> raceCars;
+
+	public Cars(List<Car> cars) {
+		this.raceCars = cars;
+	}
+
+	public void moveOneTime() {
+
+	}
+}
